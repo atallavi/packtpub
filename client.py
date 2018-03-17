4f823f260392415824763a5bdf0b2ef3ab7a6273
Arnau Tallaví <atallavi@gmail.com>
2018-03-17 21:47:01 +0100

    Fixed get_description method, added error control

--- client.py
+++ client.py
@@ -5,26 +5,34 @@ import urllib2
 class Client(object):
 
     def get_html(self, web_url):
-        f = urllib2.urlopen(web_url)
-        html = f.read()
-        f.close
+        try :
+            f = urllib2.urlopen(web_url)
+        except urllib2.URLError as e:
+            print(e)
+            exit(0)
+        try:
+            html = f.read()
+            f.close
+        except IOError as e:
+            print (e)
+            exit(0)
         return html
 
     def get_name(self, html):
         bs = bs4.BeautifulSoup(html,"html.parser")
-        name = bs.find("div", class_="dotd-title").find("h2").text
-        if name == None:
+        name = bs.find("div", class_="dotd-title").text
+        if name is None:
             return "No book available"
         else:
             return name.replace("\t", "").replace("\n", "")
 
-
     def get_description(self, html):
-        bs = bs4.BeautifulSoup(html, "html.parser")
-        description = bs.find("div", "dotd-main-book-summary float-left").text
-        return description.replace("\t", "").replace("\n",
-                "").replace("Time is running out to claim this free ebook",
-                "").replace("Building Recommendation Engines", "")
+        bs = bs4.BeautifulSoup(html, "lxml")
+        description = bs.find("div", class_="dotd-main-book-summary float-left").find("div", class_="").text
+        if description is None:
+            return "No description available"
+        else:
+            return description.replace("\t", "").replace("\n","")
 
 
     def main(self):
@@ -32,7 +40,7 @@ class Client(object):
         html = self.get_html('https://www.packtpub.com/packt/offers/free-learning/')
         name = self.get_name(html)
         description = self.get_description(html)
-        print ("Today's book in packtpub is: " + name + "\nDescription: \n" + description)
+        print ("Today's book in packtpub is: " + name + "\nDescription: " + description)
 
 
 
